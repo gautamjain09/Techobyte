2a39ac5bb3f239a5bf995882827a581263fbda25
Gautam Jain <gautamjain0918@gmail.com>
2021-08-29 04:24:11 +0530

    User Post UI

---
+++ app/src/main/java/com/gautamjain/techobyte/Adapter/PostAdapter.java
@@ -0,0 +1,92 @@
+package com.gautamjain.techobyte.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gautamjain.techobyte.Modal.Post;
+import com.gautamjain.techobyte.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hendraanggrian.appcompat.widget.SocialTextView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.Viewholder>{
+
+    private Context mContext;
+    private List<Post> mPost;
+    private FirebaseUser firebaseUser;
+
+    public PostAdapter(Context mContext, List<Post> mPost) {
+        this.mContext = mContext;
+        this.mPost = mPost;
+    }
+
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.post_item, parent, false);
+        return new PostAdapter.Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.Viewholder holder, int position) {
+
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        Post post = mPost.get(position);
+        Picasso.get().load(post.getImageurl()).into(holder.imagePost);
+        holder.description.setText(post.getDescription());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder{
+
+        public ImageView profileImage;
+        public ImageView imagePost;
+        public ImageView like;
+        public ImageView comment;
+        public ImageView save;
+        public ImageView more;
+
+        public TextView username;
+        public TextView no_of_likes;
+        public TextView author;
+        public TextView no_of_comments;
+        public SocialTextView description;
+
+        public Viewholder(View itemView) {
+            super(itemView);
+
+            profileImage = itemView.findViewById(R.id.profile_image);
+            imagePost = itemView.findViewById(R.id.id_Image_post);
+            like = itemView.findViewById(R.id.id_like);
+            save = itemView.findViewById(R.id.id_save);
+            comment = itemView.findViewById(R.id.id_comment);
+            more = itemView.findViewById(R.id.id_more);
+
+            username = itemView.findViewById(R.id.username_post);
+            no_of_likes = itemView.findViewById(R.id.id_no_of_likes);
+            no_of_comments = itemView.findViewById(R.id.id_no_of_Comments);
+            author = itemView.findViewById(R.id.id_authorname);
+            description = itemView.findViewById(R.id.description);
+
+        }
+    }
+
+}
